f5c0d1d557671647437b670ce68a287462cab4eb
Azmi <games.kdk@gmail.com>
2018-11-25 22:14:27 +0800

    template script

---
+++ 3D Sportsman vs 2D Zombie/Assets/Scripts/Enum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// All are subject to change. Some are just future planning
+// If not needed in future, will remove it
+
+public enum CharacterType
+{
+    Golf,
+    Badminton,
+    Bowling,
+    Hoop
+}
+
+public enum ZombieType
+{
+    Melee,
+    Range,
+    Boss
+}
+
+
+public class Enum : MonoBehaviour {
+
+}
---
+++ 3D Sportsman vs 2D Zombie/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Chase()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void ApplyDamage()
+    {
+
+    }
+
+    public void Dead()
+    {
+
+    }
+}
